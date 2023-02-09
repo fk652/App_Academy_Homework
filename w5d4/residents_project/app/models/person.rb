85788fbae128f3210a50f3e9c2e72143189c1f28
Fahim Khan <fk652@nyu.edu>
2023-02-09 10:36:25 -0500

    update: w5d4

--- w5d4/residents_project/app/models/person.rb
+++ w5d4/residents_project/app/models/person.rb
@@ -10,11 +10,11 @@
 #
 
 class Person < ActiveRecord::Base
-    belongs_to :house,
-      class_name: 'House',
-      foreign_key: :house_id,
-      primary_key: :id,
-      optional: true
-    
-    validates :name, presence: true
+  belongs_to :house,
+    class_name: 'House',
+    foreign_key: :house_id,
+    primary_key: :id,
+    optional: true
+  
+  validates :name, presence: true
 end
