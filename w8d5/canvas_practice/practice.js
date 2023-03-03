ae3eb59c3a78cc20a4133b5f41030158956c878c
Fahim Khan <fk652@nyu.edu>
2023-03-02 22:22:44 -0500

    finish canvas

--- w8d5/canvas_practice/practice.js
+++ w8d5/canvas_practice/practice.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById("mycanvas");
+  const ctx = canvas.getContext("2d");
 
+  // canvas.style.height = "500px";
+  // canvas.style.width = "500px";
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect(100, 100, 300, 100);
+
+  ctx.beginPath();
+  ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+  ctx.fillStyle = 'green';
+  ctx.fill();
 });
