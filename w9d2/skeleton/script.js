5b75a11629e96b7bcaed2078b48867b39102b76a
Fahim Khan <fk652@nyu.edu>
2023-03-06 22:24:43 -0500

    finish w9d2

--- w9d2/skeleton/script.js
+++ w9d2/skeleton/script.js
@@ -19,13 +19,52 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
 
+    const newLi = document.createElement("li");
+    newLi.textContent = favorite;
+
+    const sfPlaces = document.getElementById("sf-places");
+    sfPlaces.appendChild(newLi);
+  };
+  const submitButton = document.querySelector(".favorite-submit");
+  submitButton.addEventListener("click", handleSubmit);
 
   // adding new photos
 
   // --- your code here!
+  const showPhotoForm = (e) => {
+    const photoForm = document.querySelector(".photo-form-container");
+    if (photoForm.className === "photo-form-container") {
+      photoForm.className = "photo-form-container hidden";
+    } else {
+      photoForm.className = "photo-form-container";
+    }
+  };
+  const photoFormButton = document.querySelector(".photo-show-button");
+  photoFormButton.addEventListener("click", showPhotoForm);
 
+  const handlePhotoSubmit = (e) => {
+    e.preventDefault();
 
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
 
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImg);
+
+    const dogPhotosList = document.querySelector(".dog-photos");
+    dogPhotosList.appendChild(newPhotoLi);
+  };
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", handlePhotoSubmit);
 });
