bebc2abd547ccbd9e478ca583765378c50e99b3a
Fahim Khan <fk652@nyu.edu>
2023-03-07 21:42:19 -0500

    answer w9d3 questions

--- w9d3/practice-for-ch-js-first-ajax-main/index.js
+++ w9d3/practice-for-ch-js-first-ajax-main/index.js
@@ -19,3 +19,27 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4
   })
 
 console.log("The AJAX request has been dispatched.")
+
+/* 
+When does the request get sent?
+  In the fetch request (line 4)
+
+When does the response come back?
+  asynchronously through a promise in the first then statement (line 5)
+
+What's the current weather in London?
+  light snow
+
+Did the page refresh?
+  no
+
+How could you use a different HTTP method in your request?
+  by adding an object as the 2nd parameter in the fetch request, that includes:
+  header, body, headers, etc.
+
+  ex: fetch('url', {
+    method: 'POST',
+    body: ...
+    headers: {...}
+  })
+*/
