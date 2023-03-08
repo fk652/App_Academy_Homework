65233582ba4cca2265b5241524b0c5432141036d
Fahim Khan <fk652@nyu.edu>
2023-03-07 21:35:01 -0500

    finish w9d3

--- w9d3/practice-for-ch-js-first-ajax-main/index.js
+++ w9d3/practice-for-ch-js-first-ajax-main/index.js
@@ -1,3 +1,21 @@
 console.log("Hello from the console!");
 
 // Your code here
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b')
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response);
+    }
+  })
+  .then((data) => {
+    console.log("Success!");
+    console.log(data);
+  },
+  (error) => {
+    console.log("Failure!");
+    console.error(error);
+  })
+
+console.log("The AJAX request has been dispatched.")
